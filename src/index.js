69d3170ab3f0ac6fa9341e7e24f57a52a4b0c46e
Daryn Bang <daryntribang@gmail.com>
2025-06-15 11:08:20 +0700

    Test

--- src/index.js
+++ src/index.js
@@ -34,3 +34,9 @@ route(app);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
