4f5747ac5186671fedab56be4ae30be70f62972c
Thienphuc <mac@MacBook-Air-cua-MAC.local>
2025-07-02 13:37:00 +0700

    rename and deploy

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,7 +4,7 @@ async function connect() {
     try {
         // await mongoose.connect('mongodb://localhost:27017/f8_education_dev');
         await mongoose.connect(
-            'mongodb+srv://lenguyenthienphuc2004:76k8v9LqAXDg9tsu@cluster0.yeymzya.mongodb.net/',
+            'mongodb+srv://lenguyenthienphuc2004:76k8v9LqAXDg9tsu@cluster0.yeymzya.mongodb.net/CVProject',
         );
         // await mongoose.connect('mongodb+srv://bangtieudaryn1803:test1234>@cluster0.5ygzaj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         // await mongoose.connect('mongodb+srv://tribang:tribang123@clusterresume.rm2tvyc.mongodb.net/CV?retryWrites=true&w=majority&appName=ClusterResume');
